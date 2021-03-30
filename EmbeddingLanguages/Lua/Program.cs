0cb6a84e993ab8d6b20b46bf6e6d00dc8048da3f
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-03-30 16:30:48 +0300

    Add library and hello world

--- EmbeddingLanguages/Lua/Program.cs
+++ EmbeddingLanguages/Lua/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.IO;
 
 namespace Lua {
-    internal static class Program {
+    public static class Program {
+        private const string resourcesDirectoryName = "resources";
+        private static readonly string currentDirectory = Environment.CurrentDirectory;
+        private static readonly string resources = Path.Combine(currentDirectory, resourcesDirectoryName);
+        
         public static void Main() {
-            Console.WriteLine("Hello World!");
+            dynamic lua = new DynamicLua.DynamicLua();
+            lua("print(\"Hello, World!\")");
         }
     }
 }
