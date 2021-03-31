7d90d5a2dd2208376584adae5da931b94cc388f7
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-03-31 10:58:06 +0300

    Strip down projects to basic initialization

--- EmbeddingLanguages/Lua/Program.cs
+++ EmbeddingLanguages/Lua/Program.cs
@@ -1,21 +1,15 @@
 ﻿using System;
 using System.IO;
-using Embedded.Data;
-using NLua;
 
 namespace Lua {
     public static class Program {
-        private const string resourcesDirectoryName = "resources";
-        private static readonly string currentDirectory = Environment.CurrentDirectory;
-        private static readonly string resources = Path.Combine(currentDirectory, resourcesDirectoryName);
+        private static string GetResourcePath(string filePath) {
+            return $"{Path.Combine(Environment.CurrentDirectory, "resources", filePath)}";
+        }
 
         public static void Main() {
             using var lua = new NLua.Lua();
-            lua.LoadCLRPackage();
-            lua.DoFile($"{resources}/GameObject.lua");
-            lua.DoFile($"{resources}/main.lua");
-            var hello = (Vector3)(lua["CreateVector"] as LuaFunction).Call(0.5f, 2.345f, -6.9f)[0];
-            Console.WriteLine(hello);
+            lua.DoString("print('Hello, World!')");
         }
     }
 }
