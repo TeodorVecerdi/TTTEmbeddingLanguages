8020997e38467284b71cec7e7284e7fda8dd7368
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-03-31 14:25:23 +0300

    Convert python measurements to more reusable methods

--- EmbeddingLanguages/Python/Program.cs
+++ EmbeddingLanguages/Python/Program.cs
@@ -3,75 +3,94 @@ using System.Collections.Generic;
 using Python.Runtime;
 
 namespace Python {
-    internal static class Program {
+    public static class PythonProgram {
         internal static void Main() {
-            // Set python dll (part of path variable)
+        }
+
+        public static void Initialize() {
             Runtime.Runtime.PythonDLL = "python39.dll";
-            int sampleCount = 1, writes = 16384;
-            var samplesNormal_Total = Measure(sampleCount, func => RunMeasuredNormal(writes, func), () => GC.GetTotalAllocatedBytes(true));
-            var samplesGC_Total = Measure(sampleCount, func => RunMeasuredGC(writes, func), () => GC.GetTotalAllocatedBytes(true));
-            var samplesNormal_TotalProc = Measure(sampleCount, func => RunMeasuredNormal(writes, func), GC.GetAllocatedBytesForCurrentThread);
-            var samplesGC_TotalProc = Measure(sampleCount, func => RunMeasuredGC(writes, func), GC.GetAllocatedBytesForCurrentThread);
-            
-            long sumNT = 0, sumGCT = 0, sumNTP = 0, sumGCTP = 0;
-            for (var i = 0; i < sampleCount; i++) {
-                sumNT += samplesNormal_Total[i];
-                sumGCT += samplesGC_Total[i];
-                sumNTP += samplesNormal_TotalProc[i];
-                sumGCTP += samplesGC_TotalProc[i];
-            }
-            double avgNT = sumNT / (double)sampleCount, avgGCT = sumGCT / (double)sampleCount, avgNTP = sumNTP / (double)sampleCount, avgGCTP = sumGCTP / (double)sampleCount;
-
-            Console.WriteLine($"samplesNormal_Total average over {sampleCount} samples => {avgNT} bytes total, {avgNT/writes} bytes/write");
-            Console.WriteLine($"samplesGC_Total average over {sampleCount} samples => {avgGCT} bytes total, {avgGCT/writes} bytes/write");
-            Console.WriteLine($"samplesNormal_TotalProc average over {sampleCount} samples => {avgNTP} bytes total, {avgNTP/writes} bytes/write");
-            Console.WriteLine($"samplesGC_TotalProc average over {sampleCount} samples => {avgGCTP} bytes total, {avgGCTP/writes} bytes/write");
         }
-
-        internal static List<TM> Measure<TM>(int sampleCount, Func<Func<TM>, TM> function, Func<TM> measureFunction) {
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            var samples = new List<TM>();
-            for (var i = 0; i < sampleCount; i++) {
-                samples.Add(function(measureFunction));
-            }
-
-            return samples;
-        }
-
-        internal static long RunMeasuredNormal(int writes, Func<long> measureFunction) {
+        
+        public static long WriteMem(int writes, Func<long> measureFunction, Action cleanupFunction) {
             long start, end;
             using (var state = Py.GIL()) {
                 using (var scope = Py.CreateScope()) {
+                    scope.Exec("a = None");
+                    cleanupFunction?.Invoke();
                     start = measureFunction();
-                    for (int i = 0; i < writes; i++) {
-                        scope.Exec($"a = {i}");
-                    }
-
+                    Write(writes, scope);
+                    cleanupFunction?.Invoke();
                     end = measureFunction();
+                }
+            }
+            return end - start;
+        }
+        
+        public static TimeSpan WriteTime(int writes, Func<DateTime> measureFunction, Action cleanupFunction) {
+            DateTime start, end;
+            using (var state = Py.GIL()) {
+                using (var scope = Py.CreateScope()) {
+                    scope.Exec("a = None");
+                    cleanupFunction?.Invoke();
+                    start = measureFunction();
+                    Write(writes, scope);
+                    cleanupFunction?.Invoke();
+                    end = measureFunction();
+                }
+            }
+            return end - start;
+        }
+
+        private static void Write(int writes, PyScope scope) {
+            for (int i = 0; i < writes; i++) {
+                scope.Set("a", i);
+            }
+        }
+        
+        public static long ReadMem(int reads, Func<long> measureFunction, Action cleanupFunction) {
+            long start, end;
+            var a = 0;
+            using (var state = Py.GIL()) {
+                using (var scope = Py.CreateScope()) {
+                    scope.Exec("a = 1");
+                    cleanupFunction?.Invoke();
+                    start = measureFunction();
+                    Read(reads, ref a, scope);
+                    
+                    cleanupFunction?.Invoke();
+                    end = measureFunction();
+                    
+                    scope.Set("a", a);
                 }
             }
 
             return end - start;
         }
 
-        internal static long RunMeasuredGC(int writes, Func<long> measureFunction) {
-            long start, end;
+        public static TimeSpan ReadTime(int reads, Func<DateTime> measureFunction, Action cleanupFunction) {
+            DateTime start, end;
+            var a = 0;
             using (var state = Py.GIL()) {
                 using (var scope = Py.CreateScope()) {
-                    GC.Collect();
-                    GC.WaitForPendingFinalizers();
+                    scope.Exec("a = 1");
+                    cleanupFunction?.Invoke();
                     start = measureFunction();
-                    for (int i = 0; i < writes; i++) {
-                        scope.Exec($"a = {i}");
-                    }
-
-                    GC.Collect();
-                    GC.WaitForPendingFinalizers();
+                    Read(reads, ref a, scope);
+                    
+                    cleanupFunction?.Invoke();
                     end = measureFunction();
+                    
+                    scope.Set("a", a);
                 }
             }
+
             return end - start;
+        }
+
+        private static void Read(int reads, ref int a, PyScope scope) {
+            for (int i = 0; i < reads; i++) {
+                a = scope.Get<int>("a");
+            }
         }
     }
 }
