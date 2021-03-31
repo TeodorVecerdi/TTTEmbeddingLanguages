851bd4365dd7f57e1fe1c6df573d91e49d236d2c
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-03-31 14:34:21 +0300

    Measure lua read/write memory usage and time

--- EmbeddingLanguages/Measurements/Program.cs
+++ EmbeddingLanguages/Measurements/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Lua;
 using Python;
 
 namespace Measurements {
@@ -12,18 +13,26 @@ namespace Measurements {
             
             PythonProgram.Initialize();
             
-            var actions = 16384;
+            var actions = 65536;
             var py_writeNormalMemory = Measure(1, (func, cleanup) => PythonProgram.WriteMem(actions, func, cleanup), memoryCleanup, null, GC.GetAllocatedBytesForCurrentThread)[0];
             var py_writeCleanupMemory = Measure(1, (func, cleanup) => PythonProgram.WriteMem(actions, func, cleanup), memoryCleanup, memoryCleanup, GC.GetAllocatedBytesForCurrentThread)[0];
             var py_readNormalMemory = Measure(1, (func, cleanup) => PythonProgram.ReadMem(actions, func, cleanup), memoryCleanup, null, GC.GetAllocatedBytesForCurrentThread)[0];
             var py_readCleanupMemory = Measure(1, (func, cleanup) => PythonProgram.ReadMem(actions, func, cleanup), memoryCleanup, memoryCleanup, GC.GetAllocatedBytesForCurrentThread)[0];
             
+            var lua_writeNormalMemory = Measure(1, (func, cleanup) => LuaProgram.WriteMem(actions, func, cleanup), memoryCleanup, null, GC.GetAllocatedBytesForCurrentThread)[0];
+            var lua_writeCleanupMemory = Measure(1, (func, cleanup) => LuaProgram.WriteMem(actions, func, cleanup), memoryCleanup, memoryCleanup, GC.GetAllocatedBytesForCurrentThread)[0];
+            var lua_readNormalMemory = Measure(1, (func, cleanup) => LuaProgram.ReadMem(actions, func, cleanup), memoryCleanup, null, GC.GetAllocatedBytesForCurrentThread)[0];
+            var lua_readCleanupMemory = Measure(1, (func, cleanup) => LuaProgram.ReadMem(actions, func, cleanup), memoryCleanup, memoryCleanup, GC.GetAllocatedBytesForCurrentThread)[0];
+
+            
             var sampleCount = 512;
             var py_writeNormalTime = Measure(sampleCount, (func, action) => PythonProgram.WriteTime(actions, func, action), memoryCleanup, null, () => DateTime.Now);
             var py_readNormalTime = Measure(sampleCount, (func, action) => PythonProgram.ReadTime(actions, func, action), memoryCleanup, null, () => DateTime.Now);
             
+            var lua_writeNormalTime = Measure(sampleCount, (func, action) => LuaProgram.WriteTime(actions, func, action), memoryCleanup, null, () => DateTime.Now);
+            var lua_readNormalTime = Measure(sampleCount, (func, action) => LuaProgram.ReadTime(actions, func, action), memoryCleanup, null, () => DateTime.Now);
         }
-        
+
         internal static List<TR> Measure<TM, TR>(int sampleCount, Func<Func<TM>, Action, TR> function, Action cleanupFunctionSelf, Action cleanupFunction, Func<TM> measureFunction) {
             var samples = new List<TR>();
             for (var i = 0; i < sampleCount; i++) {
