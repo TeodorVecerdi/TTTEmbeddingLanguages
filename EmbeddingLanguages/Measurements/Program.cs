f8a7954d3f2b41ca55e18cfb9c4f6c4f003a6c3d
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-03-31 14:27:01 +0300

    Measure python read/write memory usage and time

---
+++ EmbeddingLanguages/Measurements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Python;
+
+namespace Measurements {
+    class Program {
+        static void Main(string[] args) {
+            var memoryCleanup = new Action(() => {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            });
+            
+            PythonProgram.Initialize();
+            
+            var actions = 16384;
+            var py_writeNormalMemory = Measure(1, (func, cleanup) => PythonProgram.WriteMem(actions, func, cleanup), memoryCleanup, null, GC.GetAllocatedBytesForCurrentThread)[0];
+            var py_writeCleanupMemory = Measure(1, (func, cleanup) => PythonProgram.WriteMem(actions, func, cleanup), memoryCleanup, memoryCleanup, GC.GetAllocatedBytesForCurrentThread)[0];
+            var py_readNormalMemory = Measure(1, (func, cleanup) => PythonProgram.ReadMem(actions, func, cleanup), memoryCleanup, null, GC.GetAllocatedBytesForCurrentThread)[0];
+            var py_readCleanupMemory = Measure(1, (func, cleanup) => PythonProgram.ReadMem(actions, func, cleanup), memoryCleanup, memoryCleanup, GC.GetAllocatedBytesForCurrentThread)[0];
+            
+            var sampleCount = 512;
+            var py_writeNormalTime = Measure(sampleCount, (func, action) => PythonProgram.WriteTime(actions, func, action), memoryCleanup, null, () => DateTime.Now);
+            var py_readNormalTime = Measure(sampleCount, (func, action) => PythonProgram.ReadTime(actions, func, action), memoryCleanup, null, () => DateTime.Now);
+            
+        }
+        
+        internal static List<TR> Measure<TM, TR>(int sampleCount, Func<Func<TM>, Action, TR> function, Action cleanupFunctionSelf, Action cleanupFunction, Func<TM> measureFunction) {
+            var samples = new List<TR>();
+            for (var i = 0; i < sampleCount; i++) {
+                cleanupFunctionSelf();
+                samples.Add(function(measureFunction, cleanupFunction));
+            }
+
+            return samples;
+        }
+    }
+}
